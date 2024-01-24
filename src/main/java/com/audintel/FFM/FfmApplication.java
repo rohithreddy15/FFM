8671941b765ed93f251ecb9aee25b6935240f59e
rohithreddy <vanapatlarohith95@gmail.com>
2024-01-24 21:24:16 +0530

    ffm

---
+++ src/main/java/com/audintel/FFM/FfmApplication.java
@@ -0,0 +1,13 @@
+package com.audintel.FFM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audintel/FFM/FfmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audintel.FFM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
