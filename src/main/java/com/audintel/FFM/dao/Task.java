68f5ed3bb9b93039f2adb8f8d0cf3e5b7ea2c04d
rohithreddy <vanapatlarohith95@gmail.com>
2024-01-25 11:26:45 +0530

    task

---
+++ src/main/java/com/audintel/FFM/dao/Task.java
@@ -0,0 +1,95 @@
+package com.audintel.FFM.dao;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CollectionId;
+
+import java.util.Date;
+
+@Entity
+public class Task {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+    String description ;
+    int createdby;
+    int assigned_to;
+    boolean status;
+    int dealerid;
+    Date assigned_date;
+    Date completed_date;
+
+    public int getDealerid() {
+        return dealerid;
+    }
+
+    public void setDealerid(int dealerid) {
+        this.dealerid = dealerid;
+    }
+
+    public Date getAssigned_date() {
+        return assigned_date;
+    }
+
+    public void setAssigned_date(Date assigned_date) {
+        this.assigned_date = assigned_date;
+    }
+
+    public Date getCompleted_date() {
+        return completed_date;
+    }
+
+    public void setCompleted_date(Date completed_date) {
+        this.completed_date = completed_date;
+    }
+
+
+
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+    public int getAssigned_to() {
+        return assigned_to;
+    }
+
+    public void setAssigned_to(int assigned_to) {
+        this.assigned_to = assigned_to;
+    }
+
+
+
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+
+
+    public int getCreatedby() {
+        return createdby;
+    }
+
+    public void setCreatedby(int createdby) {
+        this.createdby = createdby;
+    }
+}
