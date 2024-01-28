48fd95f04987642737c448a91961bacfc4472365
rohithreddy <vanapatlarohith95@gmail.com>
2024-01-28 11:52:42 +0530

    added @CrossOrigin

--- src/main/java/com/audintel/controller/Plancontroller.java
+++ src/main/java/com/audintel/controller/Plancontroller.java
@@ -5,6 +5,7 @@ import com.audintel.service.Planservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/")
 public class Plancontroller {
     @Autowired
     Planservice service;
--- src/main/java/com/audintel/controller/TasksController.java
+++ src/main/java/com/audintel/controller/TasksController.java
@@ -6,6 +6,7 @@ import com.audintel.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/")
 public class TasksController {
         @Autowired
         TaskService service;
